a35c18ea92970633aebe2f2c09a320fd4a38a0af
alimalek <alianta2016@gmail.com>
2024-08-26 16:04:18 +0300

    Add product quantities to cart_summary.html page, that shows the selected quantities by client

--- cart/views.py
+++ cart/views.py
@@ -8,7 +8,8 @@ def cart_summary(request):
     # Get the cart
     cart = Cart(request)
     cart_products= cart.get_prod
-    return render(request, "cart/cart_summary.html", {'cart_products': cart_products})
+    quantities = cart.get_quants
+    return render(request, "cart/cart_summary.html", {'cart_products': cart_products, 'quantities': quantities})
 
 
 def cart_add(request):
@@ -18,10 +19,11 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         # Get Stuff
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         # lookup product in the DB
         product = get_object_or_404(Product, id=product_id)
         # save to session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
         # Get cart quantity
         cart_quantity = cart.__len__()
         # Return Response
