af93b1d55cc4878d4561f24c06344067db0a6043
alimalek <alianta2016@gmail.com>
2024-08-31 16:29:16 +0300

    feat(cart): Add user feedback messages for cart operations
    - Import `messages` from `django.contrib` in `views.py` of the cart app.
    - Add success message "Product Added to Cart..." in `cart_add` function.
    - Add `location.reload();` to the success function of AJAX in `product.html`.
    - Add success message "Cart Updated" in `cart_update` function.
    - Add warning message "Product is removed from cart..." in `cart_delete` function.

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 
 def cart_summary(request):
@@ -30,6 +31,7 @@ def cart_add(request):
         # Return Response
         # response = JsonResponse({'Product Name: ': product.name})
         response = JsonResponse({'qty': cart_quantity})
+        messages.success(request, ("Product Added to Cart..."))
         return response
 
 
@@ -41,6 +43,7 @@ def cart_delete(request):
         # call delete function in our cart
         cart.delete(product=product_id)
         response = JsonResponse({'product':product_id})
+        messages.warning(request, ("Product is removed from cart.."))
         return response
 
 
@@ -53,6 +56,7 @@ def cart_update(request):
 
         cart.update(product=product_id, quantity= product_qty)
         response = JsonResponse({'qty':product_qty})
+        messages.success(request, ("Cart Updated"))
         return response
 
 
