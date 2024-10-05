3b330ce833f88b75ed09a555de3cf55a1b107988
alimalek <alianta2016@gmail.com>
2024-10-05 18:46:56 +0300

    feat: Customer Order Models Created Order and OrderItem models in payment models.py Added them in the admin.py

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 
 class ShippingAddress(models.Model):
@@ -19,3 +20,30 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f"shipping address - {str(self.id)}"
+
+
+# Create Order model
+class Order(models.Model):
+    # Foreign Key
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    shipping_address = models.TextField()
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order - {str(self.id)}"
+
+
+# Create Order Items model
+class OrderItem(models.Model):
+    # Foreign Key
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order Item - {str(self.id)}"
